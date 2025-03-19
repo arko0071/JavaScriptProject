55b42be1627060280c95adf3cd4d4d7d2e4ef3dc
arko0071 <raysaptarshi123@gmail.com>
2025-03-19 14:39:06 +0530

    added file through file system

---
+++ codefive.js
@@ -0,0 +1,23 @@
+let age = "24fgfg"
+
+let valueInNumber1 = Number(age)
+console.log(typeof (valueInNumber1))
+console.log(valueInNumber1)
+
+
+let age1 = null
+
+let valueInNumber3 = Number(age)
+console.log(typeof (valueInNumber3))
+console.log(valueInNumber3)
+
+
+
+let age2 = undefined
+
+let valueInNumber2 = Number(age)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber2)
+
+
+
